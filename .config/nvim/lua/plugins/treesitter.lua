b03f74f9da023a2d5b1705207dc60d943c08288d
Sam <sambe247@gmail.com>
2023-04-03 08:20:03 +0200

    missed one

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua" , "go", "python", "terraform", "markdown", "markdown_inline"},
+
+  indent = { enable = true },
+
+  highlight = {
+    enable = true,
+  },
+
+}
+
+return options
